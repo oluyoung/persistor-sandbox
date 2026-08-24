30b83c75cb1090ac6d77139ddc35383e859fc188
Temi <kolugbake@hotmail.com>
2026-08-24 15:18:34 +0100

    remove old persistor

--- src/lib/store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch as useReduxDispatch, useSelector as useReduxSelector, useStore as useReduxStore } from 'react-redux';
-import type { ReduxRootStore, RootState, AppDispatch } from '.';
-
-export const useAppDispatch = useReduxDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useReduxSelector.withTypes<RootState>();
-export const useAppStore = useReduxStore.withTypes<ReduxRootStore>();
